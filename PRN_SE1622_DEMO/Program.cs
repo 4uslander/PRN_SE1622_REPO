c7cfb835d9ff96126e081a17f0628467d1847ed9
DELL <DELL@DESKTOP-T6KEP52>
2022-05-19 15:01:22 +0700

    Add project files.

---
+++ PRN_SE1622_DEMO/Program.cs
@@ -0,0 +1,14 @@
+﻿Main();
+static void Main()
+{
+    
+
+    int x =1, y = 2;
+    void Showvalue(int a)
+    {
+        Console.WriteLine($"x = {x}, a={a}");
+        x = 10;
+    }
+    Showvalue(11);
+    Console.WriteLine(x);
+}
---
+++ PRN_SE1622_PRO/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace fptedu.se1622;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello world");
+        //int a = 5, b = 6;
+        Console.WriteLine($"input a and b: ");
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int name;
+        var salary = 100.5;
+        Console.WriteLine(salary.GetType().Name);
+
+
+        dynamic age = 20;//int32
+        age = true;//bool
+
+        Console.WriteLine($"Total of a = {a} and b = {b} is: {Sum(a,b)}");
+
+        Console.ReadLine();
+    }
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
